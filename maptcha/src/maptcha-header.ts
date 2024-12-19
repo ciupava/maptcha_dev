d9221dbed3476ee9d0683476e68a3c9e430c9d0a
Stuart Lynn <stuart.lynn@gmail.com>
2024-12-19 01:54:57 +0000

    Adding reuseable header template

---
+++ maptcha/src/maptcha-header.ts
@@ -0,0 +1,38 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import logo from '../public/maptcha_logo.webp'
+
+
+@customElement('maptcha-header')
+export class MaptchaHeader extends LitElement {
+  static styles = css`
+    .header{
+      display: flex;
+      flex-direction:row;
+      justify-content: space-between;
+      algin-items:center;
+      padding:10px 0px;
+      color:darkgrey;
+    }
+    .header img{
+      width: 5rem;
+    }
+  `
+  render(){
+    return html`
+        <header class="header">
+          <img src=${logo} />
+          <h2>Maptcha</h2>
+        </header>
+    `
+  }
+}
+
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'maptcha-header': MaptchaHeader;
+  }
+}
+
+export default MaptchaHeader;
